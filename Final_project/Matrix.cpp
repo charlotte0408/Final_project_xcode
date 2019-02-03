fa4e47cda2320179862791634a3ad06bd13c4ea3
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-02-03 08:42:32 -0800

    Implement matrix construction

--- Final_project/Matrix.cpp
+++ Final_project/Matrix.cpp
@@ -9,3 +9,12 @@
 #include "Matrix.h"
 #include <iostream>
 #include <vector>
+
+Matrix :: Matrix()
+{
+    m.resize(3);
+    for (int i = 0; i <3; i++)
+        for (int j = 0; i < 3; j++)
+            m[i][j] = 0;
+}
+
