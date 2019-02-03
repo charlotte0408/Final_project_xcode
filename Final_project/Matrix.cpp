ec16c831c195aa64c3375216cda5c1ad49cdc7d8
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-02-03 09:06:33 -0800

    Implement left function

--- Final_project/Matrix.cpp
+++ Final_project/Matrix.cpp
@@ -18,3 +18,52 @@ Matrix :: Matrix()
             m[i][j] = 0;
 }
 
+bool Matrix :: left()
+{
+    bool order = false;
+    for (int i = 0; i < 3; i++)
+    {
+        vector <int> line = m[i];
+        for (int j = 0; j< 3; j++)
+        {
+            if (line[j] != line[j+1] && line[j+1] != 0)
+                continue;
+            if (line[j] == 0)
+                continue;
+            for (int k = j+1; k < 3; k++)
+            {
+                if (line[j] == line[k] && line[j] != 0)
+                {
+                    line[j] = 2 * line[j];
+                    line[k] = 0;
+                    order = true;
+                    break;
+                }
+            }
+                                             
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        vector <int> line = m[i];
+        for (int j = 0; j < 3; j++)
+        {
+            if (line[j] == 0)
+                continue;
+            else
+            {
+                for (int k = j-1; k >= 0; k--)
+                {
+                    if (line[k] == 0)
+                    {
+                        line[k] = line[j];
+                        line[k] = 0;
+                        order = true;
+                        j = k;
+                    }
+                }
+            }
+        }
+    }
+    return order;
+}
