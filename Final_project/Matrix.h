8f103643d4924ad22c2d5516b1f865f0906dada5
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-02-02 23:47:25 -0800

    Determine to use header file instead of all putting into one cpp

---
+++ Final_project/Matrix.h
@@ -0,0 +1,30 @@
+//
+//  Matrix.h
+//  Final_project
+//
+//  Created by 黄逸欢 on 2/2/19.
+//  Copyright © 2019 Yihuan Huang. All rights reserved.
+//
+
+#ifndef Matrix_h
+#define Matrix_h
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Matrix
+{
+public:
+    Matrix();
+    bool left();
+    bool right();
+    bool up();
+    bool down();
+private:
+    vector<vector<int>> m;
+    
+};
+
+#endif /* Matrix_h */
--- Final_project/main.cpp
+++ Final_project/main.cpp
@@ -6,23 +6,6 @@
 //  Copyright © 2019 Yihuan Huang. All rights reserved.
 //
 
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-class Matrix
-{
-public:
-    Matrix();
-    bool left();
-    bool right();
-    bool up();
-    bool down();
-private:
-    vector<vector<int>> m;
-    
-};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
