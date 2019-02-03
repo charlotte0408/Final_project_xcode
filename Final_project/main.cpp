5a2a9975815cc432572831f29cb10689318da163
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-02-02 23:37:45 -0800

    Initial Commit

---
+++ Final_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final_project
+//
+//  Created by 黄逸欢 on 2/2/19.
+//  Copyright © 2019 Yihuan Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
