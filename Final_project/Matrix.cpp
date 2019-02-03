990053f66e80bcbdd63cfb3bb47ff4828bc12b8e
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-02-02 23:51:49 -0800

    Add another branch and also Matrix cpp

---
+++ Final_project/Matrix.cpp
@@ -0,0 +1,11 @@
+//
+//  Matrix.cpp
+//  Final_project
+//
+//  Created by 黄逸欢 on 2/2/19.
+//  Copyright © 2019 Yihuan Huang. All rights reserved.
+//
+
+#include "Matrix.h"
+#include <iostream>
+#include <vector>
--- Final_project/main.cpp
+++ Final_project/main.cpp
@@ -5,7 +5,7 @@
 //  Created by 黄逸欢 on 2/2/19.
 //  Copyright © 2019 Yihuan Huang. All rights reserved.
 //
-
+#include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
